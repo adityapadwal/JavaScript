1e43262e81f7eb5d5a0a243fa91dfe21b15ff4bd
Aditya-Padwal <aditya.padwal3102@gmail.com>
2024-02-07 23:12:05 +0530

    block scope and shadowing

---
+++ Block Scope and Shadowing/block.js
@@ -0,0 +1,12 @@
+{
+    // this is a block
+    // block is also called as a compound statement
+    // let and const are block scoped
+}  
+
+// Q. What is block?
+// => a block is used to combine multiple js statements into a single group
+
+// Q. Why do we use a block?
+// => We use a block where js expects at least one statement. 
+// This is because js combines multiple js statements into a single group
---
+++ Block Scope and Shadowing/shadowing.js
@@ -0,0 +1,30 @@
+var a = 100;
+{
+    var a = 10;
+    console.log(a); // outputs 10
+}
+console.log(a); // outputs 10
+
+// both outputs are 10 because both are pointing to the same memory location. 
+// this is because var declarations are function scoped, and here, both are created,
+// in the sam memory (global memory)
+
+// But things are different for let
+let b = 200;
+{
+    let b = 20;
+    console.log(b); // outputs 20
+}
+console.log(b); // outputs 200
+// this is because outer let declaration is created in global space
+// and inner let declaration is created in separate/different block space
+
+// Same is the case for const
+const c = 300;
+{
+    const c = 30;
+    console.log(c); // outputs 30
+}
+console.log(c); // outputs 300
+// this is because outer const declaration is created in global space
+// and inner const declaration is created in separate/different block space
