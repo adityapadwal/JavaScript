e90b5239d109d98f17a12b66e7aae40cea195b89
Aditya-Padwal <aditya.padwal3102@gmail.com>
2024-06-15 19:23:00 +0530

    IIF (Immediately Invoked Functions)

--- AnonymousFunctions/af2.js
+++ AnonymousFunctions/af2.js
@@ -6,6 +6,16 @@
 
 ();
 
+var result = (function() {
+	var x = 10;
+	var y = 20;
+	return x + y;
+})();
+
+console.log(result); // Output: 30
+
 // IIFE are functions that are executed as soon as they are defined
 // Here, function() { console.log('This is an IIFE.'); } 
 // is an anonymous function that is immediately invoked.
+// They are typically used to create a local scope for variables 
+// to prevent them from polluting the global scope.
